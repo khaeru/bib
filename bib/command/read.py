fcac627d817d5ce9dc0f22c7fdc94ee19b254d55
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-02-22 11:46:20 +0100

    start timewarrior clock on 'read'

--- bib/command/read.py
+++ bib/command/read.py
@@ -1,4 +1,5 @@
-import os
+from subprocess import DEVNULL, run
+from sys import stdout
 
 import click
 from click import ClickException
@@ -8,8 +9,10 @@ from ..util import pass_context
 
 @click.command('read')
 @click.argument('key')
+@click.option('--clock/--no-clock', default=False,
+              help="Start the timewarrior clock.")
 @pass_context
-def read_command(ctx, key):
+def read_command(ctx, key, clock):
     """Open the 'localfile'(s) associated with KEY."""
 
     try:
@@ -19,8 +22,11 @@ def read_command(ctx, key):
 
     try:
         for fn in entry['localfile']:
-            fn = os.path.join(ctx.config['path'], fn)
-            os.system('xdg-open "{}"'.format(fn))
+            run(['xdg-open', ctx.config['path'] / fn], stderr=DEVNULL)
     except KeyError:
         raise ClickException("entry '{}' has no localfile field."
                              .format(key))
+
+    if clock:
+        args = ['timew', 'start', 'Read', key]
+        run(args, stdout=stdout)
